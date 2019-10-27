a1f1050ded9172d146f8bb0b65d3fde5771f3d55
David <davidoregan91@gmail.com>
2019-10-27 17:47:08 +0000

    feat(blog): add blog support

--- src/components/blog/Post/index.jsx
+++ src/components/blog/Post/index.jsx
@@ -6,40 +6,26 @@ import forkIcon from 'Static/icons/fork.svg'
 import { Wrapper, Grid, Item, Content, Stats } from './styles'
 
 export const Post = () => {
-	const allMarkdownRemark = { edges: [] }
+	const {
+		markdownRemark
+		} = graphql`
+		  query($path: String!) {
+			markdownRemark(frontmatter: { path: { eq: $path } }) {
+			  html
+			  frontmatter {
+				date(formatString: "MMMM DD, YYYY")
+				path
+				title
+			  }
+			}
+		}
+	`
+
+    console.log(markdownRemark)
 	return (
 		<Wrapper as={Container} id="posts">
-			<h2>Posts</h2>
+			<h2></h2>
 			<Grid>
-				{allMarkdownRemark.edges.map(({ node }) => (
-					<Item
-						key={node.id}
-						as="a"
-						href={node.url}
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Card>
-							<Content>
-								<Link style={{ boxShadow: `none` }} to={node.frontmatter.path}>
-									<h4>{node.frontmatter.title}</h4>
-								</Link>
-								<p>{node.frontmatter.description}</p>
-								<small>{node.frontmatter.date}</small>
-							</Content>
-							<Stats>
-								<div>
-									<img src={starIcon} alt="stars" />
-									<span>0</span>
-								</div>
-								<div>
-									<img src={forkIcon} alt="forks" />
-									<span>0</span>
-								</div>
-							</Stats>
-						</Card>
-					</Item>
-				))}
 			</Grid>
 		</Wrapper>
 	)
